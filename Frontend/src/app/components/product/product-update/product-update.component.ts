6b75a3d9cde64e1f0b5339785fd421afb4cba543
Vsk Code <vynycyusvieira@gmail.com>
2021-12-30 20:41:38 -0300

    [Create] New components: Delete and Update

---
+++ Frontend/src/app/components/product/product-update/product-update.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrls: ['./product-update.component.css']
+})
+export class ProductUpdateComponent implements OnInit {
+
+  product!: Product;
+
+  constructor(
+    private productService: ProductService, 
+    private router: Router,
+    private route: ActivatedRoute
+    ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.productService.getProductById(id!).subscribe(product =>{
+      this.product = product;
+    })
+  }
+
+  updateProduct(): void{
+    this.productService.editProduct(this.product).subscribe(()=>{
+      this.productService.showMessage("Product edited successfully!");
+      this.router.navigate(['/products']);
+    })
+  }
+
+  cancel(): void{
+    this.router.navigate(['/products']);
+  }
+
+}
