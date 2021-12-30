cbd344cbc38cd0b50b793dfac6932cd7d854bb82
Vsk Code <vynycyusvieira@gmail.com>
2021-12-30 20:38:23 -0300

    [Update] Added new components in declarations

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -31,6 +31,8 @@ import { MatSortModule } from '@angular/material/sort';
 
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 registerLocaleData(localePt);
 
@@ -44,7 +46,9 @@ registerLocaleData(localePt);
     ProductCrudComponent,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent,
+    ProductDeleteComponent
   ],
   imports: [
     BrowserModule,
